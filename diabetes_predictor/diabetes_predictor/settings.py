293e741a8d436cbeefbfa0174aba2a60f26ccbdf
Mauro-World <maurojoaoluis10@gmail.com>
2022-05-23 01:13:49 +0100

    checkkpoint

--- diabetes_predictor/diabetes_predictor/settings.py
+++ diabetes_predictor/diabetes_predictor/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = os.path.join(BASE_DIR, "diabetes_predictor")
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'diabetes_predictor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- diabetes_predictor/diabetes_predictor/urls.py
+++ diabetes_predictor/diabetes_predictor/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('apoiar', views.apoiar, name="apoiar"),
+    path('register',views.register, name="register"),
+    path('login',views.login, name="login"),
+    path('diagnostico',views.diagnostico, name="diagnostico"),
+    path('base',views.base, name="base")
 ]
