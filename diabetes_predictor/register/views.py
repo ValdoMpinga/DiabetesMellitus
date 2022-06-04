5c50a3bd170e79f10bad615bf8081b1295102633
Valdo Mpinga <valdompinga57@gmail.com>
2022-06-04 18:10:45 +0100

    hotfix: frontend structure reorganized

--- diabetes_predictor/register/views.py
+++ diabetes_predictor/register/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render
-
+from .models import createUser
 def register(request):
+    if request.method == "POST":
+        print(request.POST['firstName'])
+        createUser(request.POST['firstName'])
+    
     return render(request,'register/register.html')
 
