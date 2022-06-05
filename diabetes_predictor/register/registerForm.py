f3196ff8e04fe1abab1d28c337b6410d338845d6
Valdo Mpinga <valdompinga57@gmail.com>
2022-06-05 16:24:20 +0100

    Database error bug solved!

--- diabetes_predictor/register/registerForm.py
+++ diabetes_predictor/register/registerForm.py
@@ -1,10 +1,10 @@
-# from django import forms
-# from django.forms import ModelForm
-# from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
-# from django import forms
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
-# class CreateUserForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields =['first_name','last_name', 'email','password1','password2',]
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields =['first_name','last_name', 'email','password1','password2',]
--- diabetes_predictor/register/views.py
+++ diabetes_predictor/register/views.py
@@ -1,21 +1,21 @@
 import logging
 from django.shortcuts import render, redirect
 from django.shortcuts import render
-# from .registerForm import CreateUserForm
+from .registerForm import CreateUserForm
 
 
 def register(request):
     try:
-        # form = CreateUserForm()
+        form = CreateUserForm()
 
     
-        # if request.method == 'POST':
-        #     form = CreateUserForm(request.POST)
-        #     if form.is_valid():
-        #         form.save()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
                 
-        # context = {'form': form}
-        return render(request, 'register/register.html')
+        context = {'form': form}
+        return render(request, 'register/register.html', context)
     except BaseException:
         print("-------------------------------")
         logging.exception("*Error goes here*")
