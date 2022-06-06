ff521ff83ae9b78756fa50c05906b9d25cf7ae31
Valdo Mpinga <valdompinga57@gmail.com>
2022-06-06 16:08:42 +0100

    feat: URL Params inside diagnostic views

--- diabetes_predictor/diagnostic/views.py
+++ diabetes_predictor/diagnostic/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import predictPreview
 
+
 def diagnostic(request):
     if request.method == 'POST':
         print("yo")
         predictPreview()
-        return render(request,'diagnostic/diagnostic.html')
+        return render(request, 'diagnostic/diagnostic.html')
     else:
         print("Ha")
-        return render(request,'diagnostic/diagnostic.html')
-    
+        name = {
+            "n": "Pedro"
+        }
 
-mtv = mvc
+        context = {
+            "first_name": "Naveen",
+            "last_name": "Arora",
+        }
+
+      #  return render(request, 'diagnostic/diagnostic.html', {'x': name, 'v': context})
+        return render(request, 'diagnostic/diagnostic.html', [context,name])
