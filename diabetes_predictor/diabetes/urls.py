554a3c2e1bb1ff03de5b24c7cc50b44dbda2bf78
Valdo Mpinga <valdompinga57@gmail.com>
2022-06-08 12:07:04 +0100

    feat: Diabetes page v1 done!

--- diabetes_predictor/diabetes/urls.py
+++ diabetes_predictor/diabetes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name ='diabetes'
+app_name = 'diabetes'
 
-urlpatterns =[
-    path('', views.diabetes, name='diabetes')
-] 
+urlpatterns = [
+    path('', views.diabetes, name='diabetes'),
+    path('/typeone', views.diabetesOne, name='diabetes'),
+    path('/typetwo', views.diabetesTwo, name='diabetes'),
+    path('/gestational', views.gestationalDiabetes, name='diabetes')
+]
--- diabetes_predictor/diabetes/views.py
+++ diabetes_predictor/diabetes/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def diabetes(request):
     return render(request,'diabetes/diabetes.html')
+
+def diabetesOne(request):
+    return render(request,'diabetes/typeOneDiabetes.html')
+
+def diabetesTwo(request):
+    return render(request,'diabetes/typeTwoDiabetes.html')
+
+def gestationalDiabetes(request):
+    return render(request, 'diabetes/gestationalDiabetes.html')
