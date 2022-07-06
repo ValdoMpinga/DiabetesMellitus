6854e661942ee0d421e5bd29418359c013ad7126
Valdo Mpinga <valdompinga57@gmail.com>
2022-07-06 10:23:09 +0100

    Progress bar label added

--- diabetes_predictor/static/scripts/handlers/formHandler.mjs
+++ diabetes_predictor/static/scripts/handlers/formHandler.mjs
@@ -2,7 +2,7 @@
 function handlePrematureSubmit(progress)
 {
     if (progress != 100)
-        alert('O formulário deve ser completamente preenchido antes de submetido')
+        alert('O formulário deve ser completamente preenchido antes de submetido, se por acaso chegou no final, por favor volte atrás e veja o que esta em falta.')
 }
 
 export { handlePrematureSubmit }
