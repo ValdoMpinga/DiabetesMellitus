b573e408381d1c89e9b3a3575cd5ce941f7fbc28
Valdo Mpinga <valdompinga2@gmail.com>
2023-03-19 23:23:18 +0000

    railway deploy setup

--- diabetes_predictor/diabetes_predictor/settings.py
+++ diabetes_predictor/diabetes_predictor/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'ai_trainer_condition',
     'charts',
     'ai_model_history',
+    'whitenoise.runserver_nostatic',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'diabetes_predictor.urls'
@@ -134,6 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
