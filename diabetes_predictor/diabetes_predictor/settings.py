2184eaeca2f20c26a896cdc556e6b202b78780ea
Valdo Mpinga <valdompinga2@gmail.com>
2023-03-19 19:52:40 +0000

    deploy

--- diabetes_predictor/diabetes_predictor/settings.py
+++ diabetes_predictor/diabetes_predictor/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 
 STATICFILES_DIRS = [
@@ -141,6 +142,7 @@ STATICFILES_DIRS = [
 STATICFILES_DIRS =  [
    os.path.join(BASE_DIR,'../diabetes_predictor/static')
    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
